041409a82814d5718708545482e0af12859a972c
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2021-06-05 14:25:34 +0900

    Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  ## 下記でHerokuを許可
+  config.hosts << "docker-base-oniki.herokuapp.com"
 end
